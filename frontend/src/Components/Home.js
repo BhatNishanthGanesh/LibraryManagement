14858c598e8385b5fdb2d3308497668c98d5293a
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-03-25 10:51:31 +0530

    css

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -8,8 +8,11 @@ const Home = () => {
       <div className="home-container my-5 text-black">
         <div className="row">
           <div className="col-md-8 mx-auto text-center">
-            <h1 style={{ marginTop: "20px", fontSize: "48px", fontWeight: "bold" }}>Welcome to Inventory Management</h1>
+            <center>
             <img src={inventoryImage} alt="Inventory" className="img-fluid my-5" style={{ opacity: "0" }} />
+            <h1 style={{ marginTop: "20px", fontSize: "48px", fontWeight: "bold" }}>Welcome to Inventory Management</h1>
+            </center>
+              
           </div>
         </div>
         
