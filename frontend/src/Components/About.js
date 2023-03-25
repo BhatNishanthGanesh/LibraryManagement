f0b987c06c0c7d794d01883edd582443fdfa7558
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-03-25 11:34:56 +0530

    Final

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,15 +1,23 @@
+import inventoryImage from "../images/1.png";
+
 const About = () =>{
-    return(
-        <div className="row">
-          <div className="col-md-8 mx-auto">
-            <p className="lead">
-              <b style={{ fontSize: "20px" }}>Inventory management is the process of efficiently overseeing the constant flow of units into and out of an existing inventory. This includes controlling inventory levels, ordering new products, and organizing existing inventory so that it can be accessed quickly and easily. Effective inventory management helps businesses keep their products stocked and ready for sale, while minimizing waste and reducing costs.</b>
-            </p>
-            <p className="lead">
-              <b style={{ fontSize: "20px" }}>Our inventory management system provides a centralized platform for managing your inventory. From here, you can track your inventory levels, manage your suppliers, and analyze your sales data to make informed business decisions. Use the links below to navigate to the different areas of our system:</b>
-            </p>
-          </div>
-        </div>
-    )
+  return (
+    <div className="row align-items-center">
+      <h2 style={{textAlign:"center"}}>PRAGATHI</h2>
+      <div className="col-md-4 mx-auto">
+        <img src={inventoryImage} alt="Inventory" className="img-fluid my-5 inventory-img" />
+      </div>
+      <div className="col-md-8 mx-auto">
+        <p className="lead">
+          <b style={{ fontSize: "20px" }}>Inventory management is the process of efficiently overseeing the constant flow of units into and out of an existing inventory. This includes controlling inventory levels, ordering new products, and organizing existing inventory so that it can be accessed quickly and easily. Effective inventory management helps businesses keep their products stocked and ready for sale, while minimizing waste and reducing costs.</b>
+        </p>
+        <p className="lead">
+          <b style={{ fontSize: "20px" }}>Our inventory management system provides a centralized platform for managing your inventory. From here, you can track your inventory levels, manage your suppliers, and analyze your sales data to make informed business decisions. Use the links below to navigate to the different areas of our system:</b>
+        </p>
+      </div>
+      <h3 style={{textAlign:"center"}}>Pragathi - Har Disha Mein</h3>
+    </div>
+  );
 }
+
 export default About;
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import inventoryImage from "../images/IM.jpg";
+import image1 from "../images/1.png";
+import image2 from "../images/5.png";
 import "../Css/Home.css";
 
 const Home = () => {
@@ -8,14 +10,21 @@ const Home = () => {
       <div className="home-container my-5 text-black">
         <div className="row">
           <div className="col-md-8 mx-auto text-center">
-            <center>
-            <img src={inventoryImage} alt="Inventory" className="img-fluid my-5" style={{ opacity: "0" }} />
-            <h1 style={{ marginTop: "20px", fontSize: "48px", fontWeight: "bold" }}>Welcome to Inventory Management</h1>
-            </center>
-              
+            <h1 style={{ marginTop:"40px" ,fontSize: "48px", fontWeight: "bold" }}>Welcome to Inventory Management</h1>
+            <img src={inventoryImage} alt="Inventory" className="img-fluid my-5"  />
+            <h1>From Pragathi</h1>
           </div>
         </div>
-        
+        <div className="row" style={{ display: "flex", justifyContent: "space-between",paddingLeft:"18em" }}>
+  <div className="col-md-6">
+    <img src={image1} alt="Image 1" className="img-fluid my-5 rounded-circle" style={{ width: "200px", height: "200px" }} />
+  </div>
+  <div className="col-md-6">
+    <img src={image2} alt="Image 2" className="img-fluid my-5 rounded-circle" style={{ width: "200px", height: "200px" }} />
+  </div>
+</div>
+
+
       </div>
     </>
   );
