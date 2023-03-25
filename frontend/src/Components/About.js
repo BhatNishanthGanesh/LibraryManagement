691466c7d1e98a0e84537c352514741a2d8e8d33
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-03-25 10:53:10 +0530

    Css

---
+++ frontend/src/Components/About.js
@@ -0,0 +1,15 @@
+const About = () =>{
+    return(
+        <div className="row">
+          <div className="col-md-8 mx-auto">
+            <p className="lead">
+              <b style={{ fontSize: "20px" }}>Inventory management is the process of efficiently overseeing the constant flow of units into and out of an existing inventory. This includes controlling inventory levels, ordering new products, and organizing existing inventory so that it can be accessed quickly and easily. Effective inventory management helps businesses keep their products stocked and ready for sale, while minimizing waste and reducing costs.</b>
+            </p>
+            <p className="lead">
+              <b style={{ fontSize: "20px" }}>Our inventory management system provides a centralized platform for managing your inventory. From here, you can track your inventory levels, manage your suppliers, and analyze your sales data to make informed business decisions. Use the links below to navigate to the different areas of our system:</b>
+            </p>
+          </div>
+        </div>
+    )
+}
+export default About;
