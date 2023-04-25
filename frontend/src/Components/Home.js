60c0f3c8d9b9a2a27a3b6dd420067ee72d7f1b1b
Anudeep K K <anudeep2003@gmail.com>
2023-04-25 18:29:51 +0530

    Simple

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
-import inventoryImage from "../images/IM.jpg";
+import LibraryImage from "../images/Library.jpg";
 import image1 from "../images/1.png";
 import image2 from "../images/5.png";
+
 import "../Css/Home.css";
 
 const Home = () => {
@@ -10,18 +11,18 @@ const Home = () => {
       <div className="home-container my-5 text-black">
         <div className="row">
           <div className="col-md-8 mx-auto text-center">
-            <h1 style={{ marginTop:"40px" ,fontSize: "48px", fontWeight: "bold" }}>Welcome to Inventory Management</h1>
-            <img src={inventoryImage} alt="Inventory" className="img-fluid my-5"  />
-            <h1>From Pragathi</h1>
+            <h1 style={{ marginTop:"40px" ,fontSize: "48px", fontWeight: "bold" }}>Welcome to our Library Management</h1>
+            <img src={LibraryImage} alt="Inventory" className="img-fluid my-5" style={{height:"200px"}} />
+            <h3 style={{color:"red"}}> Our library management system is designed to make it easy for you to find and borrow books. With our user-friendly interface and extensive collection of books, you'll be sure to find something to read in no time.</h3>
           </div>
         </div>
         <div className="row" style={{ display: "flex", justifyContent: "space-between",paddingLeft:"18em" }}>
-  <div className="col-md-6">
-    <img src={image1} alt="Pragathi" className="img-fluid my-5 rounded-circle" style={{ width: "200px", height: "200px" }} />
+  {/* <div className="col-md-6">
+    <img src={image1} alt="Image 1" className="img-fluid my-5 rounded-circle" style={{ width: "100px", height: "100px" }} />
   </div>
   <div className="col-md-6">
-    <img src={image2} alt="Pragathi" className="img-fluid my-5 rounded-circle" style={{ width: "200px", height: "200px" }} />
-  </div>
+    <img src={image2} alt="Image 2" className="img-fluid my-5 rounded-circle" style={{ width: "100px", height: "100px" }} />
+  </div> */}
 </div>
 
 
